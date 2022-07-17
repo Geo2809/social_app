26d8e72fcb44acd76bdafbee2786a4cd77a8092d
George <george.9409@yahoo.ro>
2022-07-17 18:54:33 +0300

    Implement delete and update functionality in posts app.

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.create_post_comment_and_list_view, name='main-post-view'),
-    path('liked/', views.like_unlike_post, name='like-post-view')
+    path('liked/', views.like_unlike_post, name='like-post-view'),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(), name='post-delete-view'),
+    path('update/<int:pk>/', views.PostUpdateView.as_view(), name='post-update-view'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import redirect, render
-
+from django.urls import reverse_lazy
 from profiles.models import Profile
 from .models import Post, Like
 from .forms import PostModelForm, CommentModelForm
+from django.views.generic import UpdateView, DeleteView
+
 
 def create_post_comment_and_list_view(request):
     qs = Post.objects.all()
@@ -10,7 +13,6 @@ def create_post_comment_and_list_view(request):
     post_form = PostModelForm()
     comment_form = CommentModelForm()
     post_added = False
-    comment_added = False
     if 'submit_post_form' in request.POST:
         post_form = PostModelForm(request.POST, request.FILES)
         if post_form.is_valid():
@@ -64,3 +66,33 @@ def like_unlike_post(request):
         post_obj.save()
         like.save()
     return redirect('posts:main-post-view')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'posts/post_delete.html'
+    success_url = reverse_lazy('posts:main-post-view')
+
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        post = Post.objects.get(pk=pk)
+        if not post.author.user == self.request.user:
+            messages.warning(self.request, "You're not the author of the post, you can't delete it.")
+        return post
+
+
+class PostUpdateView(UpdateView):
+    form_class = PostModelForm
+    model = Post
+    template_name = 'posts/post_update.html'
+    success_url = reverse_lazy('posts:main-post-view')
+
+    def form_valid(self, form):
+        profile = Profile.objects.get(user=self.request.user)
+
+        if form.instance.author == profile:
+            return super().form_valid(form)
+        else:
+            form.add_error(None, "You're not the author of the post, you can't update it.")
+            return super().form_invalid(form)
