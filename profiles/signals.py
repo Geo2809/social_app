46d24ea46d8ae75e06c37b01a441c41f8cc14b68
George <george.9409@yahoo.ro>
2022-07-18 21:19:20 +0300

    Update signals.py, add pre_delete_remove_from_friends

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,16 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile, Relationship
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def post_save_create_profile(sender, *args, **kwargs):
+def post_save_create_profile(sender, **kwargs):
     if kwargs['created']:
         Profile.objects.create(user=kwargs['instance'])
 
 
 @receiver(post_save, sender=Relationship)
-def post_save_add_to_friends(sender, instance, created, *args, **kwargs):
+def post_save_add_to_friends(sender, instance, created, **kwargs):
     sender_ = instance.sender
     receiver_ = instance.receiver
     if instance.status == 'accepted':
@@ -18,3 +18,12 @@ def post_save_add_to_friends(sender, instance, created, *args, **kwargs):
         receiver_.friends.add(sender_.user)
         sender_.save()
         receiver_.save()
+        
+@receiver(pre_delete, sender=Relationship)
+def pre_delete_remove_from_friends(sender, instance, **kwargs):
+    sender = instance.sender
+    receiver = instance.receiver
+    sender.friends.remove(receiver.user)
+    receiver.friends.remove(sender.user)
+    sender.save()
+    receiver.save()
